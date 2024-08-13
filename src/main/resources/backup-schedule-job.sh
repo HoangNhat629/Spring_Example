87b464dba69b3520ce740887b68be5523f7c2244
tay.luong <tay.luong@zen8labs.com>
2024-08-13 16:53:20 +0700

    update shell script backup

--- src/main/resources/backup-schedule-job.sh
+++ src/main/resources/backup-schedule-job.sh
@@ -17,6 +17,8 @@ echo "database: ${database}"
 # Execute backup database
 mysqldump -h ${hostname} -u ${user} -p${password} ${database} | gzip > feednexa-$(date +%F).sql.gz
 
+sleep 1
+
 # Check file exists or not
 if [ -e feednexa-$(date +%F).sql.gz ]
 then
@@ -24,5 +26,15 @@ then
 else
   echo "Backup was failure"
 fi
+
+sleep 1
+
+# Connect remote server
+sshpass -p '88Q7M3PTwUL5fM' scp feednexa-$(date +%F).sql.gz root@66.94.125.167:/backup/feednexa-$(date +%F).sql.gz
+
+sleep 1
+
+# Remove file backup
+rm -rf feednexa-$(date +%F).sql.gz
 
 exit
